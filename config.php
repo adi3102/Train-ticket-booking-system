a33c741b2927ac4d0e4b52309b143596a8bca64b
adi3102 <159716870+adi3102@users.noreply.github.com>
2025-01-22 20:23:31 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$title = 'TravelTix';
+
+$supervisor_name = "Dr. Preeti Rathi";
+$developer_name = "Rahul Agarwal, Nirbhik Pratap Singh, Aditya Sharma, Tushar pant";
+$developer_matric = "16/52HA016";
